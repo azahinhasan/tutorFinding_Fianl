767d78248fd067e8572399787f3c100c57e6b342
Ashhak Hasan Zahin <azahinhasan@gmail.com>
2020-09-14 11:32:51 +0600

    a

--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 // Initialize the session
 session_start();
@@ -12,18 +12,4 @@ session_destroy();
 // Redirect to login page
 header("location: LoginTutor.php");
 exit;
-=======
-<?php
-// Initialize the session
-session_start();
-
-// Unset all of the session variables
-$_SESSION = array();
-
-// Destroy the session.
-session_destroy();
-
-// Redirect to login page
-header("location: LoginTutor.php");
-exit;
->>>>>>> 8400874d831066dfad64911929c94ee7e9d508cf
+?>
